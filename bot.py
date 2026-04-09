7b184a07e16ad018e358de5e2cb14a0eefac8fe6
andreizloy96-tech <andreizloy96@gmail.com>
2026-04-09 20:38:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -184,4 +184,4 @@ async def on_ready():
     print(f"Синхронизировано {len(synced)} команд.")
 
 if __name__ == "__main__":
-    bot.run("MTQ5MTUwODMwMDI3NjMwNjEzNA.GbR_43.cA-8elWpSXtoHyJVj7RTLyBQLM6fFr2o-7eO-w")
+    bot.run(BOT_TOKEN)
